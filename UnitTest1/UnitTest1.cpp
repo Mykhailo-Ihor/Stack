2606fb151386b0cc4f132cc2d71fb0080d8b3894
Михайло-Ігор Проць <MykhailoIhor.Prots@lnu.edu.ua>
2024-03-05 17:58:33 +0200

    UPD

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -7,77 +7,85 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace StackUnitTest
 {
-	TEST_CLASS(StackTest)
-	{
-	public:
-		TEST_METHOD(PushPopTest)
+		TEST_CLASS(StackTest)
 		{
-			Stack myStack;
-			myStack.push(5);
-			Assert::AreEqual(5, myStack.top());
-			myStack.pop();
-			Assert::IsTrue(myStack.isEmpty());
-		}
+		public:
+			TEST_METHOD(PushPopTest)
+			{
+				Stack myStack;
+				myStack.push(5);
+				Assert::AreEqual(5, myStack.top());
+				myStack.pop();
+				Assert::IsTrue(myStack.isEmpty());
+			}
+			TEST_METHOD(DestructorTest)
+			{
+				Stack myStack;
+				myStack.push(5);
+				myStack.push(10);
+				Assert::AreEqual(myStack.top(), 10);
+				myStack.~Stack();
+				Assert::IsTrue(myStack.isEmpty());
+			}
+			TEST_METHOD(TopTest)
+			{
+				Stack myStack;
+				myStack.push(10);
+				Assert::AreEqual(10, myStack.top());
+				myStack.push(20);
+				Assert::AreEqual(20, myStack.top());
+			}
 
-		TEST_METHOD(TopTest)
-		{
-			Stack myStack;
-			myStack.push(10);
-			Assert::AreEqual(10, myStack.top());
-			myStack.push(20);
-			Assert::AreEqual(20, myStack.top());
-		}
+			TEST_METHOD(IsEmptyTest)
+			{
+				Stack myStack;
+				Assert::IsTrue(myStack.isEmpty());
+				myStack.push(5);
+				Assert::IsFalse(myStack.isEmpty());
+				myStack.pop();
+				Assert::IsTrue(myStack.isEmpty());
+			}
 
-		TEST_METHOD(IsEmptyTest)
-		{
-			Stack myStack;
-			Assert::IsTrue(myStack.isEmpty());
-			myStack.push(5);
-			Assert::IsFalse(myStack.isEmpty());
-			myStack.pop();
-			Assert::IsTrue(myStack.isEmpty());
-		}
+			TEST_METHOD(CopyConstructorTest)
+			{
+				Stack myStack;
+				myStack.push(5);
+				myStack.push(10);
+				myStack.push(15);
 
-		TEST_METHOD(CopyConstructorTest)
-		{
-			Stack myStack;
-			myStack.push(5);
-			myStack.push(10);
-			myStack.push(15);
+				Stack copyStack(myStack);
+				Assert::AreEqual(myStack.top(), copyStack.top());
+				myStack.pop();
+				copyStack.pop();
+				Assert::AreEqual(myStack.top(), copyStack.top());
+			}
+			TEST_METHOD(ExceptionTest)
+			{
+				Stack myStack;
+				Assert::ExpectException<std::runtime_error>([&] { myStack.top(); });
+				Assert::ExpectException<std::runtime_error>([&] { myStack.pop(); });
+			}
+			TEST_METHOD(CompareStacks)
+			{
+				Stack myStack;
+				std::stack<int> stlStack;
 
-			Stack copyStack(myStack);
-			Assert::AreEqual(myStack.top(), copyStack.top());
-			myStack.pop();
-			copyStack.pop();
-			Assert::AreEqual(myStack.top(), copyStack.top());
-		}
-		TEST_METHOD(ExceptionTest)
-		{
-			Stack myStack;
-			Assert::ExpectException<std::runtime_error>([&] { myStack.top(); });
-			Assert::ExpectException<std::runtime_error>([&] { myStack.pop(); });
-		}
-		TEST_METHOD(CompareStacks)
-		{
-			Stack myStack;
-			std::stack<int> stlStack;
+				myStack.push(5);
+				stlStack.push(5);
+				Assert::AreEqual(stlStack.top(), myStack.top());
 
-			myStack.push(5);
-			stlStack.push(5);
-			Assert::AreEqual(stlStack.top(), myStack.top());
+				myStack.push(10);
+				stlStack.push(10);
+				Assert::AreEqual(stlStack.top(), myStack.top());
 
-			myStack.push(10);
-			stlStack.push(10);
-			Assert::AreEqual(stlStack.top(), myStack.top());
+				myStack.pop();
+				stlStack.pop();
+				Assert::AreEqual(stlStack.top(), myStack.top());
 
-			myStack.pop();
-			stlStack.pop();
-			Assert::AreEqual(stlStack.top(), myStack.top());
-
-			myStack.pop();
-			stlStack.pop();
-			Assert::IsTrue(myStack.isEmpty());
-			Assert::IsTrue(stlStack.empty());
-		}
-	};
+				myStack.pop();
+				stlStack.pop();
+				Assert::IsTrue(myStack.isEmpty());
+				Assert::IsTrue(stlStack.empty());
+			}
+		};
 }
